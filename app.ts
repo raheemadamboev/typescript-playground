2a4df43ba380851784f4336ec134c2fadbd8e935
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 12:20:48 +0500

    array types

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "Raheem",
   age: 21,
+  hobbies: ["Football", "Music"]
 };
 
 console.log(`${person.name} ${person.age}`);
+
+
+for(const hobby in person.hobbies) {
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); !!! ERROR !!! type inference is working, map cant be used in string it can be used in array
+}
