996d01b48e7548b8fd4d38c2c0684f3b9066ebc1
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 11:39:12 +0500

    unpredictable functions

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(number1 : number, number2:number) {
-    return number1 + number2;
+function add(number1: number, number2: number, print: boolean) {
+  const result = number1 + number2;
+  if (print) {
+    console.log(result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 7;
 const number2 = 13.5;
+const printIt = true
 
-const result = add(number1, number2);
+const result = add(number1, number2, printIt);
 console.log(result);
