5a48302b19f5e22ca62260209ccb31e8204c8998
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 15:16:44 +0500

    function return types and void

--- app.ts
+++ app.ts
@@ -1,23 +1,14 @@
-type Sport = "football"  | "basketball"
-type Achievement = number | string
-
-function magic(sport: Sport, achievement: Achievement) {
-  if(sport === "football") {
-    if(typeof achievement === "number") {
-      return `Ronaldo scored ${achievement} goals`;
-    } else {
-      return `Ronaldo achieved ${achievement}`;
-    }
-  } else {
-    if(typeof achievement === "number") {
-      return `Lebron scored ${achievement}`;
-    } else {
-      return `Lebron achieved ${achievement}`;
-    }
-  }
+function add(number1: number, number2: number): number {
+  return number1 + number2;
 }
 
-console.log(magic("football", 891))
-console.log(magic("football", "Golden Ball"))
-console.log(magic("basketball", 50))
-console.log(magic("basketball", "Best Player Award"))
+function printResult(result: number): void {
+  console.log(`Result is ${result}`);
+}
+
+function doNothing() : undefined {
+  return;
+}
+
+printResult(add(15, 18));
+printResult(doNothing()); // strange accepting undefined
---
+++ parts/07-function-return-types-void.ts
@@ -0,0 +1,14 @@
+function add(number1: number, number2: number): number {
+    return number1 + number2;
+  }
+  
+  function printResult(result: number): void {
+    console.log(`Result is ${result}`);
+  }
+  
+  function doNothing() : undefined {
+    return;
+  }
+  
+  printResult(add(15, 18));
+  printResult(doNothing()); // strange accepting undefined
