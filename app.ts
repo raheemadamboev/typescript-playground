a42c35a746759508157b096ea69c2a0b1fa4d0b7
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 11:21:16 +0500

    using types

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('My code goes there... and yours i dont know');
+function add(number1 : number, number2:number) {
+    return number1 + number2;
+}
+
+const number1 = 7;
+const number2 = 13.5;
+
+const result = add(number1, number2);
+console.log(result);
