2a2e6f8ae0372c454fb2756178faeed41458302c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 13:05:44 +0500

    enums

--- app.ts
+++ app.ts
@@ -1,25 +1,17 @@
-const person: {
+enum Role {
+  ADMIN,
+  CEO,
+  EMPLOYEE,
+}
+
+const person : {
   name: string;
   age: number;
-  hobbies: string[];
-  role: [number, string];
+  role: Role;
 } = {
-//const person = {
   name: "Raheem",
   age: 21,
-  hobbies: ["Football", "Music"],
-  role: [7, "CEO"]
+  role: Role.CEO
 };
 
-console.log(`${person.name} ${person.age}`);
-
-//person.role.push("i feel it coming");
-//person.role[1] = 15;
-//person.role = [15, "ronaldo", "messi"]
-
-console.log(JSON.stringify(person.role));
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  //console.log(hobby.map()); !!! ERROR !!! type inference is working, map cant be used in string it can be used in array
-}
+console.log(person);
---
+++ parts/02-object-array-tulip.ts
@@ -0,0 +1,26 @@
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+  } = {
+  //const person = {
+    name: "Raheem",
+    age: 21,
+    hobbies: ["Football", "Music"],
+    role: [7, "CEO"]
+  };
+  
+  console.log(`${person.name} ${person.age}`);
+  
+  //person.role.push("i feel it coming");
+  //person.role[1] = 15;
+  //person.role = [15, "ronaldo", "messi"]
+  
+  console.log(JSON.stringify(person.role));
+  
+  for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); !!! ERROR !!! type inference is working, map cant be used in string it can be used in array
+  }
+  
