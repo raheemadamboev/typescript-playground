0d67f960429309a38ee80b5725ea0978719c9865
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-22 12:39:03 +0500

    arrays

--- app.ts
+++ app.ts
@@ -1,17 +1,23 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+// } = {
+const person = {
   name: "Raheem",
   age: 21,
-  hobbies: ["Football", "Music"]
+  hobbies: ["Football", "Music"],
+  role: [7, "CEO"]
 };
 
 console.log(`${person.name} ${person.age}`);
 
+person.role.push("i feel it coming");
+person.role[1] = 15;
 
-for(const hobby in person.hobbies) {
-    console.log(hobby.toUpperCase());
-    //console.log(hobby.map()); !!! ERROR !!! type inference is working, map cant be used in string it can be used in array
+console.log(JSON.stringify(person.role));
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //console.log(hobby.map()); !!! ERROR !!! type inference is working, map cant be used in string it can be used in array
 }
